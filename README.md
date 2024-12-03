e4d2dee62214ad82b8a8fedbe405598f3b7e4d66
Khian Calderon <ckhianvictory@gmail.com>
2024-12-03 22:31:52 +0800

    First Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# This is a Template Project
+
+Use this if you want to make a server side web project using PHP and Bootstrap.
+Bootstrap is a css library (not created by me).
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="stylesheet" href="bootstrap/dist/css/bootstrap.css" />
+    <link rel="stylesheet" href="./assets/index.css" />
+    <script src="assets/index.js"></script>
+    <title>PHP Bootstrap Template</title>
+  </head>
+  <body>
+    <?php
+		echo "Hello World, i have PHP and Bootstrap.";
+	?>
+  </body>
+</html>
